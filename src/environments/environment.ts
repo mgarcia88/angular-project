311381a7083da28a1941364738abfb67b134a983
Maiara Garcia <maiara.garcia@bevicred.com.br>
2020-07-23 22:45:15 -0300

    commit inicial

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBZlgof3zwituLDuMtmI_Lh9JKn3gy9UnM",
+    authDomain: "angular-project-38aed.firebaseapp.com",
+    databaseURL: "https://angular-project-38aed.firebaseio.com",
+    projectId: "angular-project-38aed",
+    storageBucket: "angular-project-38aed.appspot.com",
+    messagingSenderId: "61968139565",
+    appId: "1:61968139565:web:ac6ff8a030af196fd67fa4",
+    measurementId: "G-DWZZ86LFYC"
+  }
+};
